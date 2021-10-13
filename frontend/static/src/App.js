acde62b53c3b45047bc83d531642d94cb9e4f906
ggroshansii <garthgroshansii@gmail.com>
2021-10-13 18:33:53 -0400

    added all necessary components for chat app

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
-
+        <Header />
+        <Main />
+        <Footer />
     </div>
   );
 }
