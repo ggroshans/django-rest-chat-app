5039931547c6e80a619e2993c374ee631384d225
ggroshansii <garthgroshansii@gmail.com>
2021-10-13 15:00:17 -0400

    created serializers for Room and Message models

---
+++ chats/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Room, Message
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ("name",)
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ("created_at", "updated_at", "author", "body", "room")
