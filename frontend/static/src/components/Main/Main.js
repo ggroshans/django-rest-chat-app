1b60c75972a4455402d0f868c48ff4ec33e5e41d
ggroshansii <garthgroshansii@gmail.com>
2021-10-13 20:48:55 -0400

    changed ALLOWED_HOSTS

--- frontend/static/src/components/Main/Main.js
+++ frontend/static/src/components/Main/Main.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import ChatRoomList from './ChatRoomList/ChatroomList'
-import MessageList from './MessageList/MessageList'
-import { useState } from 'react'
+import React from "react";
+import ChatRoomList from "./ChatRoomList/ChatroomList";
+import MessageList from "./MessageList/MessageList";
+import { useState } from "react";
 
 export default function Main() {
+    const [chatRooms, setChatRooms] = useState([]);
+    const [messages, setMessages] = useState([]);
 
-const [chatRooms, setChatRooms] = useState([])
-const [messages, setMessages] = useState([])
-
+    function grabChatRooms() {
+        fetch("")
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    }
 
     return (
         <div>
             <ChatRoomList />
             <MessageList />
         </div>
-    )
+    );
 }
