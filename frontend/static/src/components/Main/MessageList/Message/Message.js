6625850d041395c04a7c60ee7a473c30aeb2df70
ggroshansii <garthgroshansii@gmail.com>
2021-10-15 11:19:56 -0400

    added 'edit' and 'close' buttons from react-icons; styled icon div in message and added hover styling

--- frontend/static/src/components/Main/MessageList/Message/Message.js
+++ frontend/static/src/components/Main/MessageList/Message/Message.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./Message.css"
-
+import { FaRegEdit  } from 'react-icons/fa'
+import { AiOutlineClose } from 'react-icons/ai'
 export default function Message(props) {
     return (
         <div className="message-bubble">
+            <div className="message-icon-container">
+                <button><FaRegEdit/></button>
+                <button><AiOutlineClose/></button>
+            </div>
             <span>{props.author}:</span>
             <span>{props.body}:</span>
         </div>
