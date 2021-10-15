02532aa2f530d7bee79165eece714bab1db05182
ggroshansii <garthgroshansii@gmail.com>
2021-10-14 20:26:48 -0400

    updated message view for POST request

--- chats/views.py
+++ chats/views.py
@@ -20,5 +20,11 @@ class MessageListAPIView(generics.ListCreateAPIView):
     def get_queryset(self):
         room_instance = self.kwargs['chatroom']
         return Message.objects.filter(room=room_instance)
+    
+    def perform_create(self,serializer):
+        room_instance = get_object_or_404(Room, id=self.kwargs['chatroom'])
+        serializer.save(room=room_instance)
 
+# class MessageDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+#     serializer_class = MessageSerializer
     
