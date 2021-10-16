922f365139a8c55a6613c150ee8026f4d0373caa
ggroshansii <garthgroshansii@gmail.com>
2021-10-16 17:38:43 -0400

    created handleRegistration() for posting userData after submitting registration form

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -6,11 +6,36 @@ import Footer from './components/Footer/Footer'
 import Registration from './components/User/Registration/Registration'
 
 function App() {
+
+  function handleError(err) {
+    console.warn(err)
+  }
+
+  function handleRegistration(userData) {
+    const options = {
+      method: "POST",
+      header: {
+        "Content-Type": 'application/json',
+        "X-CSRFToken": Cookies.get('csrftoken'),
+      },
+      body: JSON.stringify(userData)
+    }
+    const response = await fetch('/rest-auth/registration/', options).catch(handleError)
+    if (!response === true) {
+      console.warn(response)
+    } else {
+      const data = response.json()
+      Cookies.set(`Authorization`, `Token ${data.key}`)
+    }
+  }
+
+
+
   return (
     <div className="App">
         <Header />
         {/* <Main /> */}
-        <Registration/>
+        <Registration handleRegistration={handleRegistration}/>
         <Footer />
     </div>
   );
