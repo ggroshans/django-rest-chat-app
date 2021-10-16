9471275ed96d8a82e597708f4e374218a7a8811c
ggroshansii <garthgroshansii@gmail.com>
2021-10-16 18:05:19 -0400

    added warning if password do not match; fixed errors in registration.js; imported js-cookie in App.js

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -1,44 +1,44 @@
+import "./App.css";
+import Cookies from 'js-cookie';
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
+import Registration from "./components/User/Registration/Registration";
 
-import './App.css';
-import Header from './components/Header/Header'
-import Main from './components/Main/Main'
-import Footer from './components/Footer/Footer'
-import Registration from './components/User/Registration/Registration'
 
 function App() {
-
-  function handleError(err) {
-    console.warn(err)
-  }
-
-  function handleRegistration(userData) {
-    const options = {
-      method: "POST",
-      header: {
-        "Content-Type": 'application/json',
-        "X-CSRFToken": Cookies.get('csrftoken'),
-      },
-      body: JSON.stringify(userData)
+    function handleError(err) {
+        console.warn(err);
     }
-    const response = await fetch('/rest-auth/registration/', options).catch(handleError)
-    if (!response === true) {
-      console.warn(response)
-    } else {
-      const data = response.json()
-      Cookies.set(`Authorization`, `Token ${data.key}`)
+
+   async function handleRegistration(userData) {
+        const options = {
+            method: "POST",
+            header: {
+                "Content-Type": "application/json",
+                "X-CSRFToken": Cookies.get("csrftoken"),
+            },
+            body: JSON.stringify(userData),
+        };
+        const response = await fetch("/rest-auth/registration/", options).catch(
+            handleError
+        );
+        if (!response === true) {
+            console.warn(response);
+        } else {
+            const data = response.json();
+            Cookies.set(`Authorization`, `Token ${data.key}`);
+        }
     }
-  }
 
-
-
-  return (
-    <div className="App">
-        <Header />
-        {/* <Main /> */}
-        <Registration handleRegistration={handleRegistration}/>
-        <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            {/* <Main /> */}
+            <Registration handleRegistration={handleRegistration} />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- frontend/static/src/components/User/Registration/Registration.js
+++ frontend/static/src/components/User/Registration/Registration.js
@@ -1,52 +1,85 @@
-
-import React from 'react'
-import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from "react";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Registration(props) {
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password1: "",
+        password2: "",
+    });
 
-const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password1: "",
-    password2: "",
-})
-
-function handleChange(e) {
-    const modifiedData = {
-        ...formData
+    function handleChange(e) {
+        const modifiedData = {
+            ...formData,
+        };
+        modifiedData[e.target.id] = e.target.value;
+        setFormData(modifiedData);
+        console.log(formData.username);
     }
-    modifiedData[e.target.id] = e.target.value
-    setFormData(modifiedData)
-    console.log(formData.username)
-}
+    let passwordWarning;
+    if (formData.password1 !== formData.password2){
+        passwordWarning = <p className='text-danger'>Passwords do not match</p>
+    } else {
+        passwordWarning = ""
+    }
 
-function handleSubmit(e) {
-    props.handleRegistration()
-}
+    function handleSubmit(e) {
+        e.preventDefault()
+        if (formData.password1 === formData.password2){
+            props.handleRegistration(formData); 
+        }
+
+
+    }
 
     return (
         <div>
-            <form>
+            <form onSubmit={(e) => handleSubmit(e)}>
                 <div className="form-group text-left mb-3 mt-5">
                     <label htmlFor="username">Username:</label>
-                    <input type="text" id="username" value={formData.username} onChange={(e) => handleChange(e)} placeholder="Enter Username"/>
+                    <input
+                        type="text"
+                        id="username"
+                        value={formData.username}
+                        onChange={(e) => handleChange(e)}
+                        placeholder="Enter Username"
+                    />
                 </div>
                 <div className="form-group text-left mb-3 mt-5">
                     <label htmlFor="email">Email:</label>
-                    <input type="text" id="email" value={formData.email} onChange={(e) => handleChange(e)} placeholder="Enter Email"/>
+                    <input
+                        type="text"
+                        id="email"
+                        value={formData.email}
+                        onChange={(e) => handleChange(e)}
+                        placeholder="Enter Email"
+                    />
                 </div>
                 <div className="form-group text-left mb-3 mt-5">
                     <label htmlFor="Password">Password:</label>
-                    <input type="text" id="password" value={formData.password} onChange={(e) => handleChange(e)} placeholder="Enter Password"/>
+                    <input
+                        type="text"
+                        id="password1"
+                        value={formData.password1}
+                        onChange={(e) => handleChange(e)}
+                        placeholder="Enter Password"
+                    />
                 </div>
                 <div className="form-group text-left mb-3 mt-5">
                     <label htmlFor="password">Password:</label>
-                    <input type="text" id="password" value={formData.password} onChange={(e) => handleChange(e)} placeholder="Enter Password"/>
+                    <input
+                        type="text"
+                        id="password2"
+                        value={formData.password2}
+                        onChange={(e) => handleChange(e)}
+                        placeholder="Enter Password"
+                    />
+                    {passwordWarning}
                 </div>
-                <button onSubmit={(e) => handleSubmit(e)}>Register</button>
+                <button type="submit">Register</button>
             </form>
         </div>
-    )
+    );
 }
