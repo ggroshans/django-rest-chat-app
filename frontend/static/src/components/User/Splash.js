4096cfd3e17035d2276dbd5c55c491f00083d2b0
ggroshansii <garthgroshansii@gmail.com>
2021-10-17 16:25:10 -0400

    relatively positioned buttons on splash screen

--- frontend/static/src/components/User/Splash.js
+++ frontend/static/src/components/User/Splash.js
@@ -10,12 +10,15 @@ export default function Splash(props) {
         <div className='splash-container'>
             <h1>Welcome to <span className="diskord-green">Diskord</span> Chat Client</h1>
             <p>Diskord is a place where you can belong to a programming cohort, a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
+            <div className='splash-btn-group'>
             <button className="btn btn-success" value="register" onClick={handleClick}>
                 Register
             </button>
             <button className="btn btn-success mt-2" value="login" onClick={handleClick}>
                 Login
             </button>
+            </div>
+
         </div>
     );
 }
