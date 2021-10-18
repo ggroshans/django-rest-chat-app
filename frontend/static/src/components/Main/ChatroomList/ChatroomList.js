52995e29c38b0aa97ace87c547b283b4a1b7e614
ggroshansii <garthgroshansii@gmail.com>
2021-10-17 20:45:13 -0400

    styled ChatRoomList component

--- frontend/static/src/components/Main/ChatroomList/ChatroomList.js
+++ frontend/static/src/components/Main/ChatroomList/ChatroomList.js
@@ -3,6 +3,7 @@ import React from "react";
 import ChatRoom from "./ChatRoom/ChatRoom";
 import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
+import { MdAddCircle } from "react-icons/md"
 
 export default function ChatRoomList(props) {
     const [addChatFlag, setAddChatFlag] = useState(false);
@@ -13,8 +14,8 @@ export default function ChatRoomList(props) {
     }
     function handleAddChatName() {
         props.postChatRoom(newChatName);
-        setNewChatName("")
-        setAddChatFlag(false)
+        setNewChatName("");
+        setAddChatFlag(false);
     }
 
     function handleChange(e) {
@@ -25,27 +26,29 @@ export default function ChatRoomList(props) {
 
     if (addChatFlag) {
         html = (
-            <div>
+            <div className='create-chatroom-container'>
                 <input
                     onChange={(e) => handleChange(e)}
                     value={newChatName}
                     type="text"
                 />
-                <button onClick={(e) => handleAddChatName()}>Add Name</button>
+                <button onClick={(e) => handleAddChatName()} className="btn btn-success-outline create-chat-btn">Create</button>
             </div>
         );
     } else {
         html = (
-            <button onClick={(e) => handleAddChat(e)}> + Add Chat Room </button>
+            <button onClick={(e) => handleAddChat(e)} className="btn btn-success-outline add-chatroom-btn"> <MdAddCircle/> Add Channel </button>
         );
     }
 
     return (
         <div className="chatroom-list-container">
-            Chat Rooms:
-            {html}
+            <div className="chatroom-list-header">
+                <h2>Channels:</h2>
+                {html}
+            </div>
+            <div className="chatroom-list">
             {props.chatRooms.map((chatRoom) => {
-                console.log("chatRoom", chatRoom);
                 return (
                     <ChatRoom
                         {...chatRoom}
@@ -55,6 +58,8 @@ export default function ChatRoomList(props) {
                     />
                 );
             })}
+            </div>
+
         </div>
     );
 }
